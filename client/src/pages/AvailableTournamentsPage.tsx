136fc89c0e6a043b4c95cd0b78c24bb64fb6e3fd
Alessandro Flori <alessandro.flori@proton.me>
2026-02-05 19:54:04 +0100

    Fix pdf download route on frontend

--- client/src/pages/AvailableTournamentsPage.tsx
+++ client/src/pages/AvailableTournamentsPage.tsx
@@ -4,7 +4,7 @@ import { TournamentData, Tournaments } from './RetrieveTournamentPage';
 import { Page } from '@/components/Page.tsx';
 import { useLocation } from 'react-router-dom';
 import { useAuth } from '@/components/AuthProvider';
-import { isTMA } from '@tma.js/sdk-react';
+import { postEvent } from '@tma.js/sdk-react';
 import { DisplayData } from '@/components/DisplayData/DisplayData';
 
 export const AvailableTournamentsPage: FC = () => {
@@ -13,7 +13,6 @@ export const AvailableTournamentsPage: FC = () => {
 
   const onClick = (tournamentData: TournamentData) => {
 
-    // This triggers the browser's native download manager
     const apiRequest = fetch(
       `/api/tournament/generate-link`,
       {
@@ -25,21 +24,22 @@ export const AvailableTournamentsPage: FC = () => {
         body: JSON.stringify(tournamentData),
       }
     )
-    Promise.all([isTMA('complete'), apiRequest]).then(([_, response]) => {
-      response.json().then(({ token }) => {
-        const url = `${window.location.origin}/api/tournament/download?token=${token}`
-        window.location.href = url;
-
+    apiRequest.then((response) => {
+      response.json().then(({ user, token }) => {
+        const url = `${window.location.origin}/api/tournament/download?user=${user}&token=${token}`
+        if (import.meta.env.DEV) {
+          window.location.href = url;
+        } else {
+          postEvent('web_app_request_file_download', {
+            url: url,
+            file_name: `${tournamentData.date}_${tournamentData.name}.pdf`
+          })
+        }
       }
       )
-
     });
-
-
   }
 
-  // Cast the state to our interface. 
-  // We use optional chaining because state could be 'null' if someone types the URL directly.
   const { date, tournaments } = location.state as Tournaments;
   return (
     <Page>
@@ -51,7 +51,7 @@ export const AvailableTournamentsPage: FC = () => {
               .map((tournamentData) => (
                 {
                   title: tournamentData.name,
-                  value: <span onClick={() => onClick(tournamentData)}> 
+                  value: <span onClick={() => onClick(tournamentData)}>
                     Participants: {tournamentData.teams.length}
                   </span>
                 }
