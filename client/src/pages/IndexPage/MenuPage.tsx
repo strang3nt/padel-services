c29e1126db4a91c173acab294c2b23bde0349e98
Alessandro Flori <alessandro.flori@proton.me>
2026-03-01 13:58:12 +0100

    Fix main menu index links

--- client/src/pages/IndexPage/MenuPage.tsx
+++ client/src/pages/IndexPage/MenuPage.tsx
@@ -1,32 +1,33 @@
-import { type FC } from 'react';
+import { type FC } from "react";
 
-import { Link } from '@/components/Link/Link.tsx';
-import { Page } from '@/components/Page.tsx';
+import { Link } from "@/components/Link/Link.tsx";
+import { Page } from "@/components/Page.tsx";
 
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import Section from '@/components/Section';
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Section from "@/components/Section";
 export const MenuPage: FC = () => (
   <Page back={false}>
     <Section title="Menu">
       <List>
-        <ListItemButton>
-          <Link to="/create-tournament">
+        <Link to="/create-tournament">
+          <ListItemButton>
             <ListItemText
               primary="Create tournament"
-              secondary="Input data to create tournament" />
-          </Link>
-        </ListItemButton>
-
-        <ListItemButton>
-          <Link to="/retrieve-tournament">
+              secondary="Input data to create tournament"
+            />
+          </ListItemButton>
+        </Link>
+        <Link to="/retrieve-tournament">
+          <ListItemButton>
             <ListItemText
               primary="Retrieve tournament"
-              secondary="Retrieve tournament pairings and, if available, results" />
-          </Link>
-        </ListItemButton>
+              secondary="Retrieve tournament pairings and, if available, results"
+            />
+          </ListItemButton>
+        </Link>
       </List>
     </Section>
-  </Page >
+  </Page>
 );
